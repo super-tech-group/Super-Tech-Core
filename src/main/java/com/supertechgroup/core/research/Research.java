376cae28aeebc01441bc420bdff6fdc046c8e8f8
warreo <warcoding@gmail.com>
2019-04-10 14:36:48 -0500

    - Started Research Files

---
+++ src/main/java/com/supertechgroup/core/research/Research.java
@@ -0,0 +1,20 @@
+package com.supertechgroup.core.research;
+
+public class Research {
+	
+	private double InspirationChance;
+	private int researchProcceses;
+	private String Requirments;
+
+	public double getInspirationChance() {
+		return InspirationChance;
+	}
+	
+	public int getResearchProcceses() {
+		return researchProcceses;
+	}
+	
+	public String getRequirments() {
+		return Requirments;
+	}
+}
---
+++ src/main/java/com/supertechgroup/core/research/Researchable.java
@@ -0,0 +1,13 @@
+package com.supertechgroup.core.research;
+
+import java.util.ArrayList;
+
+public class Researchable{
+	
+	private ArrayList<Research> research;
+	
+	public void addResearch(Research r) {
+		research.add(r);
+	}
+
+}

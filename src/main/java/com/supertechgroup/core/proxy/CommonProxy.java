9a805d3362e5b9f6553628e2c52cd989548ef5ad
oa10712 <oa10712xbox@gmail.com>
2019-04-11 13:15:00 -0500

    Removal of vanilla items implimented

--- src/main/java/com/supertechgroup/core/proxy/CommonProxy.java
+++ src/main/java/com/supertechgroup/core/proxy/CommonProxy.java
@@ -2,8 +2,12 @@ package com.supertechgroup.core.proxy;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import com.google.common.collect.Lists;
 import com.supertechgroup.core.Config;
+import com.supertechgroup.core.ModRegistry;
 import com.supertechgroup.core.Reference;
 import com.supertechgroup.core.metallurgy.Material;
 import com.supertechgroup.core.network.PacketHandler;
@@ -12,10 +16,12 @@ import com.supertechgroup.core.worldgen.WorldGenEvents;
 import com.supertechgroup.core.worldgen.generators.WorldGeneratorBase;
 import com.supertechgroup.core.worldgen.ores.Ore;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockStone;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
@@ -25,7 +31,9 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.registries.ForgeRegistry;
 
 public abstract class CommonProxy {
 	public static Configuration config;
@@ -39,11 +47,21 @@ public abstract class CommonProxy {
 
 	public World getWorld() {
 		return getWorld(null);
-	}
+	} 
 
 	public abstract World getWorld(IBlockAccess world);
 
 	public void init(FMLInitializationEvent event) {
+		// remove unwanted vanilla items
+		ForgeRegistry<IRecipe> recipeRegistry = (ForgeRegistry<IRecipe>) ForgeRegistries.RECIPES;
+		List<Item> disabledItems = Arrays.asList(ModRegistry.disabledVanillaItems);
+		for (IRecipe r : Lists.newArrayList(recipeRegistry)) {
+			if (disabledItems.contains(r.getRecipeOutput().getItem())) {
+				recipeRegistry.remove(r.getRegistryName());
+			}
+		}
+
+		// setup geology
 		vanillaReplace.add(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.STONE));
 		vanillaReplace
 				.add(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.ANDESITE));
--- src/main/java/com/supertechgroup/core/research/ResearchEvents.java
+++ src/main/java/com/supertechgroup/core/research/ResearchEvents.java
@@ -1,10 +1,14 @@
 package com.supertechgroup.core.research;
 
+import com.supertechgroup.core.ModRegistry;
+import com.supertechgroup.core.Reference;
 import com.supertechgroup.core.integration.jei.JEIMainPlugin;
 import com.supertechgroup.core.items.MaterialTool;
 import com.supertechgroup.core.metallurgy.Material;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 
@@ -25,5 +29,16 @@ public class ResearchEvents {
 			JEIMainPlugin.handleItemBlacklisting(new ItemStack(m.getItemShovel(), 1, MaterialTool.SHOVEL), true);
 			JEIMainPlugin.handleItemBlacklisting(new ItemStack(m.getItemAxe(), 1, MaterialTool.AXE), true);
 		});
+
+		// hide disable vanilla stuff
+		for (Item i : ModRegistry.disabledVanillaItems) {
+			JEIMainPlugin.handleItemBlacklisting(new ItemStack(i), true);
+		}
+
+//Unhide some basic stuff
+
+		Material wood = Material.REGISTRY.getValue(new ResourceLocation(Reference.MODID + ":wood"));
+		JEIMainPlugin.handleItemBlacklisting(new ItemStack(wood.getItemHammer(), 1, MaterialTool.HAMMER), false);
+
 	}
 }

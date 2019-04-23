71332fb109d919110043dd33defb0a53e9e7aca7
Allen Odden <oa10712xbox@gmail.com>
2019-04-22 21:30:03 -0500

    fixed a bug with opening the heater

--- src/main/java/com/supertechgroup/core/machinery/multiblock/crudeheater/CrudeHeaterBlock.java
+++ src/main/java/com/supertechgroup/core/machinery/multiblock/crudeheater/CrudeHeaterBlock.java
@@ -1,6 +1,7 @@
 package com.supertechgroup.core.machinery.multiblock.crudeheater;
 
 import com.supertechgroup.core.Reference;
+import com.supertechgroup.core.machinery.multiblock.TileMultiBlock;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
@@ -8,8 +9,11 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
@@ -59,6 +63,16 @@ public class CrudeHeaterBlock extends Block implements ITileEntityProvider {
 	@Override
 	public TileEntity createNewTileEntity(World worldIn, int meta) {
 		return new CrudeHeaterTileEntity();
+	}
+
+	@Override
+	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand,
+			EnumFacing side, float hitX, float hitY, float hitZ) {
+		if (!world.isRemote) {
+			CrudeHeaterTileEntity te = (CrudeHeaterTileEntity) world.getTileEntity(pos);
+			return te.onActivate(player, side);
+		}
+		return true;
 	}
 
 	public void registerModels() {
--- src/main/java/com/supertechgroup/core/machinery/multiblock/crudeio/CrudeIOBlock.java
+++ src/main/java/com/supertechgroup/core/machinery/multiblock/crudeio/CrudeIOBlock.java
@@ -1,22 +1,25 @@
 package com.supertechgroup.core.machinery.multiblock.crudeio;
 
 import com.supertechgroup.core.Reference;
-import com.supertechgroup.core.machinery.multiblock.BlockMultiWall;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 
-public class CrudeIOBlock extends BlockMultiWall implements ITileEntityProvider {
+public class CrudeIOBlock extends Block implements ITileEntityProvider {
 	public CrudeIOBlock() {
 		super(Material.ROCK);
 		this.setUnlocalizedName(Reference.MODID + ".crudeIO");
@@ -59,6 +62,16 @@ public class CrudeIOBlock extends BlockMultiWall implements ITileEntityProvider 
 	@Override
 	public TileEntity createNewTileEntity(World worldIn, int meta) {
 		return new CrudeIOTileEntity();
+	}
+
+	@Override
+	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand,
+			EnumFacing side, float hitX, float hitY, float hitZ) {
+		if (!world.isRemote) {
+			CrudeIOTileEntity te = (CrudeIOTileEntity) world.getTileEntity(pos);
+			return te.onActivate(player, side);
+		}
+		return true;
 	}
 
 	public void registerModels() {
